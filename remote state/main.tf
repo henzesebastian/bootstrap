bc55ea70af91015ab450949f0e5cb715a21f1dbc
henzesebastian <61322984+henzesebastian@users.noreply.github.com>
2025-12-05 11:04:39 +0100

    Add Azure resources for storage and state management

--- remote state/main.tf
+++ remote state/main.tf
@@ -1,1 +1,26 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  https_traffic_only_enabled = true   # enforce HTTPS
+
+  public_network_access_enabled = false
+  allow_nested_items_to_be_public = false
+  min_tls_version = "TLS1_2"
+}
+
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
 
